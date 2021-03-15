9b46efe1423c9036217dd6c974875d85009495a8
LouisRavillon <LouisRavillon>
2021-03-15 19:51:23 +0100

    modify train function + add patient id in topk

--- models/mil_model.py
+++ models/mil_model.py
@@ -2,20 +2,25 @@ from pytorch_lightning.core.lightning import LightningModule
 from pytorch_lightning.metrics.functional.classification import accuracy, auroc, precision_recall
 import torch
 import torch.nn as nn
+import pandas as pd
+from pathlib import Path
+from sklearn.metrics import balanced_accuracy_score
 
-from core.data.samplers import TopKSampler
-from core.distributed.ops import all_gather_op
-from core.processing import TopKProcessor
+from data.samplers import TopKSampler
+from distributed.ops import all_gather_op
+from processing import TopKProcessor
 
 
 class MILModel(LightningModule):
-    def __init__(self, model, topk=2, aggregation='max'):
+    def __init__(self, model, topk=2, aggregation='max', output_dir=''):
         super(MILModel, self).__init__()
         self.model = model
         self.loss = nn.BCELoss()
         self.topk_processor = TopKProcessor(topk=topk, aggregation=aggregation)
         self.training_log_step, self.validation_log_step, self.testing_log_step = 0, 0, 0
         self.training_log_epoch, self.validation_log_epoch, self.testing_log_epoch = 0, 0, 0
+        self.output_dir = output_dir
+        self.compute_test_metrics = True
 
     def forward(self, image):
         return self.model(image)
@@ -35,24 +40,58 @@ class MILModel(LightningModule):
     def training_epoch_end(self, outputs):
         outputs = self.all_gather_outputs(outputs)
         loss, indices, probs, labels = outputs
-        self.trainer.datamodule.train_dataset_reference.dataset.loc[indices, 'trained_prob'] = probs
-        probs, preds, labels = self.topk_processor.aggregate(
+        self.trainer.datamodule.train_dataset_reference.dataset.loc[indices.cpu(), 'trained_prob'] = probs.cpu()
+        _, probs, preds, labels = self.topk_processor.aggregate(
             self.trainer.datamodule.train_dataset_reference.dataset,
-            indices,
-            prob_col_name='prob',
+            indices.cpu(),
+            prob_col_name='trained_prob',
             group='id'
         )
-        acc, auc, precision, recall = self.get_metrics(probs, preds, labels)
+        acc, balanced_acc, auc, precision, recall = self.get_metrics(probs, preds, labels)
         self.logger.log_metrics(
             {
                 f'Training/Epoch {k}': v for k, v in
-                {'acc': acc, 'auc': auc, 'precision': precision, 'recall': recall}.items()
+                {'acc': acc, 'balanced_acc': balanced_acc, 'auc': auc, 'precision': precision, 'recall': recall}.items()
             },
             self.training_log_epoch
         )
         self.training_log_epoch += 1
-        self.training_metrics = {'acc': acc, 'auc': auc, 'precision': precision, 'recall': recall}
+        self.training_metrics = {'acc': acc, 'balanced_acc': balanced_acc, 'auc': auc, 'precision': precision, 'recall': recall}
 
+    def validation_step(self, batch, batch_idx):
+        index, image, label = batch
+        output = torch.sigmoid(self(image))
+        loss = self.loss(output, label.float())
+        self.logger.log_metrics({'Validation/Step Loss': loss}, step=self.validation_log_step)
+        self.validation_log_step += 1
+        return {'index': index, 'prob': output, 'label': label, 'loss': loss}
+
+    def validation_epoch_end(self, outputs):
+        outputs = self.all_gather_outputs(outputs)
+        loss, indices, probs, labels = outputs
+        self.trainer.datamodule.validation_dataset_reference.dataset.loc[indices.cpu(), 'validation_prob'] = probs.cpu()
+        self.topk_indices = self.topk_processor(
+            self.trainer.datamodule.validation_dataset_reference.dataset,
+            prob_col_name='validation_prob',
+            group='id'
+        )
+        patient_ids, probs, preds, labels = self.topk_processor.aggregate(
+            self.trainer.datamodule.validation_dataset_reference.dataset,
+            self.topk_indices,
+            prob_col_name='validation_prob',
+            group='id'
+        )
+        acc, balanced_acc, auc, precision, recall = self.get_metrics(probs, preds, labels)
+        self.logger.log_metrics(
+            {
+                f'Validation/Epoch {k}': v for k, v in
+                {'acc': acc, 'balanced_acc': balanced_acc, 'auc': auc, 'precision': precision, 'recall': recall}.items()
+            },
+            self.validation_log_epoch
+        )
+        self.validation_log_epoch += 1
+        self.validation_metrics = {'acc': acc, 'balanced_acc': balanced_acc, 'auc': auc, 'precision': precision, 'recall': recall}
+    
     def test_step(self, batch, batch_idx):
         index, image, label = batch
         output = torch.sigmoid(self(image))
@@ -64,29 +103,36 @@ class MILModel(LightningModule):
     def test_epoch_end(self, outputs):
         outputs = self.all_gather_outputs(outputs)
         loss, indices, probs, labels = outputs
-        self.trainer.datamodule.inference_dataset_reference.dataset.loc[indices, 'prob'] = probs
+        self.trainer.datamodule.inference_dataset_reference.dataset.loc[indices.cpu(), 'inference_prob'] = probs.cpu()
         self.topk_indices = self.topk_processor(
             self.trainer.datamodule.inference_dataset_reference.dataset,
-            prob_col_name='prob',
+            prob_col_name='inference_prob',
             group='id'
         )
-        probs, preds, labels = self.topk_processor.aggregate(
+        patient_ids, probs, preds, labels = self.topk_processor.aggregate(
             self.trainer.datamodule.inference_dataset_reference.dataset,
             self.topk_indices,
-            prob_col_name='prob',
+            prob_col_name='inference_prob',
             group='id'
         )
-        acc, auc, precision, recall = self.get_metrics(probs, preds, labels)
-        self.logger.log_metrics(
-            {
-                f'Testing/Epoch {k}': v for k, v in
-                {'acc': acc, 'auc': auc, 'precision': precision, 'recall': recall}.items()
-            },
-            self.testing_log_epoch
-        )
-        self.testing_log_epoch += 1
-        self.trainer.datamodule.train_sampler = TopKSampler(self.topk_indices)
-        return {'acc': acc, 'auc': auc, 'precision': precision, 'recall': recall}
+        names = ['id', 'probs', 'preds', 'label']
+        data = [patient_ids, probs.numpy(), preds.numpy(), labels.numpy()]
+        inference_df = pd.DataFrame.from_dict(dict(zip(names, data)))
+        inference_df.to_csv(Path(self.output_dir, f'inference.csv'))
+        if self.compute_test_metrics:
+          acc, balanced_acc, auc, precision, recall = self.get_metrics(probs, preds, labels)
+          self.logger.log_metrics(
+              {
+                  f'Testing/Epoch {k}': v for k, v in
+                  {'acc': acc, 'balanced_acc': balanced_acc, 'auc': auc, 'precision': precision, 'recall': recall}.items()
+              },
+              self.testing_log_epoch
+          )
+          self.testing_log_epoch += 1
+          self.trainer.datamodule.train_sampler = TopKSampler(self.topk_indices)
+          self.inference_metrics = {'acc': acc, 'balanced_acc': balanced_acc, 'auc': auc, 'precision': precision, 'recall': recall}
+        else:
+          self.inference_metrics = {'acc': 0}
 
     def configure_optimizers(self):
         return torch.optim.AdamW([{'params': self.model.parameters(), 'lr': 1e-3}])
@@ -97,23 +143,16 @@ class MILModel(LightningModule):
         indices = torch.cat([x['index'] for x in outputs])
         labels = torch.cat([x['label'] for x in outputs])
 
-        if 'CPU' in self.trainer.accelerator_backend.__class__.__name__:
-            return (
+        return (
                 losses.mean(),
                 indices,
                 probs,
                 labels
-            )
-
-        return (
-            all_gather_op(losses).mean(),
-            all_gather_op(indices),
-            all_gather_op(probs),
-            all_gather_op(labels)
         )
-
+        
     def get_metrics(self, probs, preds, labels):
         acc = accuracy(preds, labels)
+        balanced_acc = balanced_accuracy_score(labels.numpy(), preds.numpy())        
         auc = auroc(probs, labels)
         precision, recall = precision_recall(preds, labels)
-        return acc.item(), auc.item(), precision.item(), recall.item()
+        return acc.item(), balanced_acc.item(), auc.item(), precision.item(), recall.item()
--- train.py
+++ train.py
@@ -1,34 +1,54 @@
 from pytorch_lightning import seed_everything, Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
 
-from core.callbacks import ProgressBar
-from core.data.data_module import PCAMDataModule
-from core.models.mil_model import MILModel
-from core.models.simple_cnn import SimpleCNN
-
+from callbacks import ProgressBar
+from data.data_module import DataModule
+from models.mil_model import MILModel
+from models.simple_cnn import SimpleCNN
+from models.encoder import se_resnet50
 
 def run_training(clf, trainer, data_module):
     assert trainer.reload_dataloaders_every_epoch
-    print('Setting trainer.max_epochs = 1.')
     trainer.max_epochs = 1
     data_module.inference_dataset_reference = data_module.train_dataset
-    inference_metrics = trainer.test(clf, datamodule=data_module)[0]
+    trainer.test(clf, datamodule=data_module)
+    inference_metrics = clf.inference_metrics
     data_module.train_dataset_reference = data_module.train_dataset
+    data_module.validation_dataset_reference = data_module.val_dataset
     trainer.fit(clf, datamodule=data_module)
     training_metrics = clf.training_metrics
-    return inference_metrics, training_metrics
+    validation_metrics = clf.validation_metrics
+    return inference_metrics, training_metrics, validation_metrics
 
 
 if __name__ == '__main__':
     seed_everything(31)
 
-    data_module = PCAMDataModule('/Users/dilip.thiagarajan/data/pcam/', 128, 0, 48)
+    data_module = LymphoDataModule(
+        data_dir='data/3md3070-dlmi/',
+        batch_size=16, 
+        num_workers=0)
     data_module.setup()
+    
+    checkpoint_callback = ModelCheckpoint(
+        monitor='val_loss',
+        dirpath='checkpoints',
+        filename='{epoch:02d}-{val_loss:.2f}',
+        save_last=True,
+        mode='auto'
+    )
 
     model = SimpleCNN()
     clf = MILModel(model)
-    trainer = Trainer(reload_dataloaders_every_epoch=True, callbacks=[ProgressBar()])
+    trainer = Trainer(
+        reload_dataloaders_every_epoch=True, 
+        check_val_every_n_epoch=1,
+        gpus=1,
+        callbacks=[ProgressBar(), checkpoint_callback])
 
     all_training_metrics = []
+    all_validation_metrics = []
     for epoch in range(5):
-        inference_metrics, training_metrics = run_training(clf, trainer, data_module)
+        inference_metrics, training_metrics, validation_metrics = run_training(clf, trainer, data_module)
         all_training_metrics.append(training_metrics)
+        all_validation_metrics.append(validation_metrics)
